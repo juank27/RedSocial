39b298ed52e7b10bc4b87fa07911126cb43538c6
juank27 <juankruiz27@gmail.com>
2022-08-23 11:27:26 -0500

    Layaut y dando estilos

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ def index():
 		'semestre': 9,
 		'telefono': '331 123 4567',
 		'asignaturas': asignaturas,
+		'lenasignatura' : len(asignaturas),
 	}
    return render_template("index.html", info=info)
 
